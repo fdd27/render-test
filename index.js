49c1fd301b797c98a0851b5ce12cf965af30a7d1
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2023-12-10 15:19:49 +0200

    Added functionality for updating and deleting from db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
     console.log('Path:', request.path);
@@ -10,28 +13,9 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.static('dist'))
-// Express JSON parser used to post new entries (middleware)
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 // Initial page
 app.get('/', (request, response) => {
@@ -40,65 +24,83 @@ app.get('/', (request, response) => {
 
 // Get all
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 // Get single
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.statusMessage = `A note with the id ${id} does not exist. `
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) response.json(note)
+            else response.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
 // Delete
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 // Post
 app.post('/api/notes', (request, response) => {
     const body = request.body
-    
+
     if (!body.content) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+})
+
+// Put
+app.put('/api/notes/:id', (req, res) => {
+    const body = req.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
     }
 
-    notes = notes.concat(note)
-    
-    response.json(note)
+    // new: true returns the new updated document, otherwise you get the previous one by default
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then(updatedNote => res.json(updatedNote))
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+// Error handler has to be the last loaded middleware
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
