d2f1f8207c643fd8c28faa535159303ffbc21150
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2023-12-11 11:52:08 +0200

    Installed eslint and fixed all errors

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method);
-    console.log('Path:', request.path);
-    console.log('Body:', request.body);
-    console.log('---');
-    next()
+	console.log('Method:', request.method)
+	console.log('Path:', request.path)
+	console.log('Body:', request.body)
+	console.log('---')
+	next()
 }
 
 app.use(express.static('dist'))
@@ -19,90 +19,90 @@ app.use(cors())
 
 // Initial page
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+	response.send('<h1>Hello World!</h1>')
 })
 
 // Get all
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
 })
 
 // Get single
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) response.json(note)
-            else response.status(404).end()
-        })
-        .catch(error => next(error))
+	Note.findById(request.params.id)
+		.then(note => {
+			if (note) response.json(note)
+			else response.status(404).end()
+		})
+		.catch(error => next(error))
 })
 
 // Delete
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 // Post
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+	if (body.content === undefined) {
+		return response.status(400).json({ error: 'content missing' })
+	}
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+	})
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+	note.save()
+		.then(savedNote => {
+			response.json(savedNote)
+		})
+		.catch(error => next(error))
 })
 
 // Put
-app.put('/api/notes/:id', (req, res) => {
-    const { content, important } = req.body
+app.put('/api/notes/:id', (req, res, next) => {
+	const { content, important } = req.body
 
-    // new: true returns the new updated document, otherwise you get the previous one by default
-    Note.findByIdAndUpdate(
-        req.params.id,
-        { content, important },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedNote => res.json(updatedNote))
-        .catch(error => next(error))
+	// new: true returns the new updated document, otherwise you get the previous one by default
+	Note.findByIdAndUpdate(
+		req.params.id,
+		{ content, important },
+		{ new: true, runValidators: true, context: 'query' }
+	)
+		.then(updatedNote => res.json(updatedNote))
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+	console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return res.status(400).send({ error: error.message })
-    }
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	}
+	else if (error.name === 'ValidationError') {
+		return res.status(400).send({ error: error.message })
+	}
 
-    next(error)
+	next(error)
 }
 // Error handler has to be the last loaded middleware
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
